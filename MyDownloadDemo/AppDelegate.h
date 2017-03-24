fa311ec89040b07cc603071f99b5cd7464faee69
renjinbo <renjinbo@navercorp.com>
2017-03-24 14:16:37 +0800

    Initial Commit

---
+++ MyDownloadDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDownloadDemo
+//
+//  Created by jimbo on 2017/3/24.
+//  Copyright © 2017年 naver. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MyDownloadDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MyDownloadDemo
+//
+//  Created by jimbo on 2017/3/24.
+//  Copyright © 2017年 naver. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
